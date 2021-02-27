59dc10bbbd6e916f07d32b5c54d35438067e6698
Malyuta Aleksey <overby999@gmail.com>
2021-02-27 22:26:21 +0300

    fast push

--- scripts/build.cjs
+++ scripts/build.cjs
@@ -2,7 +2,7 @@ const webpack = require('webpack');
 const helpersConfigs = require('../packages/helpers/webpack.config.cjs');
 const objectPathConfigs = require('../packages/object-path/webpack.config.cjs');
 const coreConfigs = require('../packages/core/webpack.config.cjs');
-const reactQueryWrapperConfigs = require('../packages/react-query-wrapper/webpack.config.cjs');
+const reactQueryWrapperConfigs = require('../packages/react-query/webpack.config.cjs');
 
 async function runSeq(configs) {
   for (const config of configs) {
